a8abb81ef7ff6abce4642446f3395121396108f4
Eugen <eugeniu.popa@isa.utm.md>
2021-10-26 18:35:44 +0300

    Adding Lab4

---
+++ src/OOP/lab4/ExpressionChecker.java
@@ -0,0 +1,25 @@
+package OOP.lab4;
+
+import java.util.Stack;
+
+public class ExpressionChecker {
+    public static String BracketChecker(String str) {
+        if (str.isEmpty()) return "The expression is empty";
+        Stack<Character> stack = new Stack<Character>();
+        for (char chr : str.toCharArray()) {
+            if (chr == '(' || chr == '[' || chr == '{') {
+                stack.push(chr);
+            } else if (chr == ')' || chr == ']' || chr == '}') {
+                if (stack.isEmpty()) {
+                    return "The expression is not correct";
+                }
+                if (chr == ')' && stack.peek() == '(' || chr == ']' && stack.peek() == '[' || chr == '}' && stack.peek() == '{')
+                    stack.pop();
+                else return "The expression is not correct";
+            }
+        }
+
+        if(stack.isEmpty()) return "The expression is correct";
+        else return "The expression is not correct";
+    }
+}
